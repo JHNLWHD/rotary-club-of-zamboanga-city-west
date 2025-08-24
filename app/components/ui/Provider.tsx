93437c4b53e1b50b401e6994fbcf63800b00d3d3
JHNLWHD <aljhoenilw@gmail.com>
2025-08-24 18:42:39 +0800

    chore: remove react query - we don't need it for SSR

--- app/components/ui/Provider.tsx
+++ app/components/ui/Provider.tsx
@@ -1,14 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react"
-import { QueryClientProvider } from "@tanstack/react-query"
-import { queryClient } from "../../lib/query-client"
 import system from "../../theme/rotary-theme"
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider value={system}>
-        {children}
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider value={system}>
+      {children}
+    </ChakraProvider>
   )
 }
